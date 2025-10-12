ee1761926e831e21e74104699dc4278f1b25cdef
danielsones <daniel.sones123@gmail.com>
2025-10-12 18:09:03 +0100

    chore: host kafka in docker

---
+++ docker/kafka/kafka-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+create_topic() {
+  local topic=$1
+  /usr/bin/kafka-topics --bootstrap-server localhost:9092 \
+    --create --if-not-exists \
+    --topic "$topic" \
+    --replication-factor 1 \
+    --partitions 1
+}
+
+until /usr/bin/kafka-topics --bootstrap-server localhost:9092 --list; do
+  echo "Waiting for Kafka to be ready..."
+  sleep 5
+done
+
+create_topic new-topic
