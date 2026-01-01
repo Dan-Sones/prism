8828462480aaa3cff330121137b9b02d4602d71a
danielsones <daniel.sones123@gmail.com>
2026-01-01 10:43:31 +0000

    chore: create admin-portal vite project and install tailwind

---
+++ services/admin-portal/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center ">
+      <h1 className="text-white text-2xl underline self-center">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
