6f93a212737fb2e9ce9a930aecf738169de1cf69
danielsones <daniel.sones123@gmail.com>
2026-02-08 20:07:04 +0000

    chore: document required changes to assignment logic
    After finishing reading Schultzberg et al.'s paper, I've realised my variant assignment logic is wrong. Variant assignment should be seperate from how we sample users into experiments.

--- services/assignment-service/internal/service/assignmentService.go
+++ services/assignment-service/internal/service/assignmentService.go
@@ -27,6 +27,19 @@ func NewAssignmentService(logger *slog.Logger, bService *BucketService, assignme
 func (e *AssignmentService) GetVariantsForUserId(ctx context.Context, userId string) (map[string]string, error) {
 	bucket := e.bucketService.GetBucketFor(userId)
 
+	// REQUIRED CHANGES:
+	// 1. Make a request (worry about caching later) to get the active experiments for a bucket.
+	// within this response there should be: experiment_id, salt, "flag_id", a list of varisants with their variant_id, flag_value and traffic allocation which is represented as lower_bound and upper_bound for each variant.
+	// we should assume that the upper_bound and lower_bound are between 0 and 100, and that is is not possible for there to be overlapping in these traffic allocations.
+	// 2. perform an additional md5 hashing step on the user_id + experiment_id + salt. then mod this hash value by 100 to get a number between 0 and 100.
+	// 3. compare the number from step 2 with the traffic allocation for each variant to determine which variant the user is bucketed into for each experiment.
+	// 4. return a map of flag_id to the flag_value for the variant that the user is bucketed into for each experiment.
+
+	// Will get it working as is first, and then will add back in the caching layer. I need to consider what we would cache in this new instance.
+	// I guess just the response from the gRPC call. The actual hash can then be done on the fly without the need to re-request bounds etc.
+	// We still need the same operations in the cache, but I am not sure on the structure of the cache....
+	// we currently use hmap in redis, but we would now be storing an entire object as the value, I'm not sure if this is an anti-pattern or not?
+
 	assignments, err := e.assignmentCache.GetAssignmentsForBucket(ctx, bucket)
 	if err != nil {
 		e.logger.Error("Failed to get cached assignments for bucket", "bucket", bucket, "error", err)
