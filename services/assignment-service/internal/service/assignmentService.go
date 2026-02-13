bc2083947db75b424b30da910e3ca393443c857d
danielsones <daniel.sones123@gmail.com>
2026-02-13 10:15:23 +0000

    fix: remove redundant cache writes

--- services/assignment-service/internal/service/assignmentService.go
+++ services/assignment-service/internal/service/assignmentService.go
@@ -31,19 +31,6 @@ func NewAssignmentService(logger *slog.Logger, bService *BucketService, experime
 func (e *AssignmentService) GetAssignmentsForUserId(ctx context.Context, userId string) (map[string]string, error) {
 	bucket := e.bucketService.GetBucketFor(userId)
 
-	// REQUIRED CHANGES:
-	// 1. Make a request (worry about caching later) to get the active experiments for a bucket.
-	// within this response there should be: experiment_id, salt, "flag_id", a list of varisants with their variant_id, flag_value and traffic allocation which is represented as lower_bound and upper_bound for each variant.
-	// we should assume that the upper_bound and lower_bound are between 0 and 100, and that is is not possible for there to be overlapping in these traffic allocations.
-	// 2. perform an additional md5 hashing step on the user_id + experiment_id + salt. then mod this hash value by 100 to get a number between 0 and 100.
-	// 3. compare the number from step 2 with the traffic allocation for each variant to determine which variant the user is bucketed into for each experiment.
-	// 4. return a map of flag_id to the flag_value for the variant that the user is bucketed into for each experiment.
-
-	// Will get it working as is first, and then will add back in the caching layer. I need to consider what we would cache in this new instance.
-	// I guess just the response from the gRPC call. The actual hash can then be done on the fly without the need to re-request bounds etc.
-	// We still need the same operations in the cache, but I am not sure on the structure of the cache....
-	// we currently use hmap in redis, but we would now be storing an entire object as the value, I'm not sure if this is an anti-pattern or not?
-
 	experiments, err := e.experimentCache.GetExperimentsForBucket(ctx, bucket)
 	if err != nil {
 		return nil, err
@@ -57,6 +44,16 @@ func (e *AssignmentService) GetAssignmentsForUserId(ctx context.Context, userId 
 			return nil, fmt.Errorf("failed to get experiments and variants for bucket %d: %w", bucket, err)
 		}
 		e.logger.Info("Fetched assignments from gRPC", "bucket", bucket)
+
+		go func() {
+			cacheCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			err := e.experimentCache.SetExperimentsForBucket(cacheCtx, bucket, experiments)
+			if err != nil {
+				e.logger.Error("Failed to cache experiments for bucket", "bucket", bucket, "error", err)
+			}
+		}()
 	}
 
 	assignments := make(map[string]string)
@@ -68,16 +65,6 @@ func (e *AssignmentService) GetAssignmentsForUserId(ctx context.Context, userId 
 		}
 		assignments[experiment.ExperimentKey] = variant
 	}
-
-	go func() {
-		cacheCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		err := e.experimentCache.SetExperimentsForBucket(cacheCtx, bucket, experiments)
-		if err != nil {
-			e.logger.Error("Failed to cache experiments for bucket", "bucket", bucket, "error", err)
-		}
-	}()
 
 	return assignments, nil
 }
