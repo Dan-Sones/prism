c5038f50c1c063bb34841d5d6033688473133005
danielsones <daniel.sones123@gmail.com>
2026-02-21 15:41:40 +0000

    feat: table error states

--- services/admin-portal/src/components/table/Table.tsx
+++ services/admin-portal/src/components/table/Table.tsx
@@ -3,12 +3,17 @@ import Spinner from "../spinner/Spinner";
 import TableHeader from "./TableHeader";
 import TableRow from "./TableRow";
 
-const Table = <T,>({ data, columns, loading }: TableProps<T>) => {
+const Table = <T,>({ data, columns, loading, error }: TableProps<T>) => {
+  // TODO: Table Sorting
+  // TODO: Pagination
+  // TODO: Row Actions (Edit, delete, etc.)
+  // TODO: View
+
   return (
     <div className="w-full rounded-md bg-white shadow-xs">
-      <table className="w-full [&_td]:px-3 [&_td]:py-5 [&_th]:px-4 [&_th]:py-4">
+      <table className="w-full [&_td]:px-3 [&_td]:py-5 [&_th]:px-3 [&_th]:py-4">
         <TableHeader columns={columns} />
-        {!loading && (
+        {!loading && !error && data.length > 0 && (
           <tbody>
             {data.map((row, index) => (
               <TableRow key={index} row={row} columns={columns} />
@@ -19,6 +24,17 @@ const Table = <T,>({ data, columns, loading }: TableProps<T>) => {
       {loading && (
         <div className="flex justify-center py-20">
           <Spinner />
+        </div>
+      )}
+      {error && (
+        <div className="flex flex-col items-center py-20 text-sm">
+          <p className="text-red-500">Something went wrong.</p>
+          <p className="text-gray-400">{error.message}</p>
+        </div>
+      )}
+      {!loading && !error && data.length === 0 && (
+        <div className="flex justify-center py-20 text-sm text-gray-400">
+          No results found.
         </div>
       )}
     </div>
