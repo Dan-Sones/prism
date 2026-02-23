8db33c6424b370ab7e1107cd3176a9038b89a7b1
danielsones <daniel.sones123@gmail.com>
2026-02-23 09:21:28 +0000

    feat: validate fields of incoming events

--- services/events-service/src/main/java/org/prism/eventsservice/service/EventService.java
+++ services/events-service/src/main/java/org/prism/eventsservice/service/EventService.java
@@ -1,10 +1,13 @@
 package org.prism.eventsservice.service;
 
 import io.grpc.ManagedChannel;
+import java.util.ArrayList;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.prism.eventsservice.grpc.events_catalog.v1.EventType;
 import org.prism.eventsservice.grpc.events_catalog.v1.EventsCatalogServiceGrpc;
 import org.prism.eventsservice.grpc.events_catalog.v1.GetEventTypeByKeyRequest;
+import org.prism.eventsservice.model.EventPropertiesValidationResult;
 import org.prism.eventsservice.model.EventRequest;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +21,37 @@ public class EventService {
         this.eventsCatalogStub = EventsCatalogServiceGrpc.newBlockingStub(channel);
     }
 
-    public void IngestEvent(EventRequest eventRequest) {
-        log.info("Event Ingested Bruv" + " " + eventRequest.getEventKey() + " "
-                + eventRequest.getUserDetails().getId());
+    public void IngestEvent(EventRequest eventToIngest) {
+        var eventType = lookupEventType(eventToIngest.getEventKey());
 
-        EventType eventType = eventsCatalogStub.getEventTypeByKey(GetEventTypeByKeyRequest.newBuilder()
-                .setEventKey(eventRequest.getEventKey())
-                .build());
+        var validationResult = validateEventProperties(eventToIngest.getProperties(), eventType);
+        if (!validationResult.isValid()) {
+            return;
+        }
 
-        log.info("Event Type Retrieved: " + eventType.getName());
+        log.info(
+                "Ingesting event of type " + eventType.getName() + " with properties " + eventToIngest.getProperties());
+    }
+
+    private EventPropertiesValidationResult validateEventProperties(
+            Map<String, Object> eventProperties, EventType eventType) {
+        ArrayList<String> missingFields = new ArrayList<>();
+
+        for (var schemaField : eventType.getFieldsList()) {
+            if (!eventProperties.containsKey(schemaField.getFieldKey())) {
+                // TODO: Missing fields need to be raised through an observable alert in the portal, for now just log
+                // and discard the event
+                log.warn("Missing field " + schemaField.getFieldKey() + " in event " + eventType.getName());
+                missingFields.add(schemaField.getFieldKey());
+            }
+        }
+
+        return new EventPropertiesValidationResult(missingFields.isEmpty(), missingFields);
+    }
+
+    private EventType lookupEventType(String eventKey) {
+        // TODO: First look in the redis cache, if not found then make a GRPC call and cache-aside
+        return eventsCatalogStub.getEventTypeByKey(
+                GetEventTypeByKeyRequest.newBuilder().setEventKey(eventKey).build());
     }
 }
