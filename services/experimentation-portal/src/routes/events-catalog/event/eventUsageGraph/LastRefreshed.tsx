db0ab7a3b8d86ad4c7ecbe9b427052af264766ff
danielsones <daniel.sones123@gmail.com>
2026-03-08 07:56:39 +0000

    feat: add last updated tooltip

--- services/experimentation-portal/src/routes/events-catalog/event/eventUsageGraph/LastRefreshed.tsx
+++ services/experimentation-portal/src/routes/events-catalog/event/eventUsageGraph/LastRefreshed.tsx
@@ -1,4 +1,5 @@
 import ArrowPathIcon from "../../../../components/icons/ArrowPathIcon";
+import InformationCircleIcon from "../../../../components/icons/InformationCircleIcon";
 
 interface LastUpdatedProps {
   LastUpdated: Date;
@@ -12,15 +13,25 @@ const LastUpdated = ({
   onRefresh,
 }: LastUpdatedProps) => {
   return (
-    <span className="flex flex-row items-center justify-between gap-2">
+    <span className="flex flex-row items-center gap-2">
       <button onClick={onRefresh}>
         <ArrowPathIcon
           className={`size-3 cursor-pointer text-gray-600 transition-transform duration-500 ${isLoading ? "animate-spin" : ""}`}
         />
       </button>
-      <p className="text-xs font-extralight text-gray-400">
-        Last Updated: {LastUpdated.toLocaleTimeString()}
-      </p>
+      <div className="flex gap-0.5">
+        <p className="text-xs font-extralight text-gray-400">
+          Last Updated: {LastUpdated.toLocaleTimeString()}
+        </p>
+        <div className="relative w-fit">
+          <InformationCircleIcon className="peer size-3 text-gray-400" />
+          <span className="absolute bottom-full left-1/2 w-max -translate-x-1/2 -translate-y-0.5 rounded-md bg-stone-800 px-2 py-1 text-xs text-stone-50 opacity-0 transition-all peer-hover:-translate-y-1 peer-hover:opacity-100">
+            All events are ingested in global batches of 10,000 Events. This
+            means that this graph may take some time to update after an event is
+            ingested.
+          </span>
+        </div>
+      </div>
     </span>
   );
 };
