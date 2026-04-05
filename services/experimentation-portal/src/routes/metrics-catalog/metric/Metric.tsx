d5177c7d99fbc0d2533a4e74d7a14c211c5a66c3
danielsones <daniel.sones123@gmail.com>
2026-04-05 17:16:22 +0100

    feat: metric component card

--- services/experimentation-portal/src/routes/metrics-catalog/metric/Metric.tsx
+++ services/experimentation-portal/src/routes/metrics-catalog/metric/Metric.tsx
@@ -3,6 +3,7 @@ import PageTitle from "../../../components/title/PageTitle";
 import { getMetricByKey } from "../../../api/metricsCatalog/get-metric-by-key";
 import { useQuery } from "@tanstack/react-query";
 import MetricDetails from "./MetricDetails";
+import MetricComponentCard from "./MetricComponentCard";
 
 const Metric = () => {
   const params = useParams<{ metric_key: string }>();
@@ -29,6 +30,14 @@ const Metric = () => {
           isLoading={isMetricDetailsLoading}
           isError={isMetricDetailsError}
         />
+        <div className="flex min-w-full flex-col gap-4 xl:flex-row">
+          <p>Components</p>
+          {metricDetails?.metric_components.map((component) => (
+            // TODO: When we have ratio metrics developed, we will need to have a better way of displaying this stuff
+            // Also the order needs to be consistent
+            <MetricComponentCard key={component.id} component={component} />
+          ))}
+        </div>
       </div>
     </>
   );
