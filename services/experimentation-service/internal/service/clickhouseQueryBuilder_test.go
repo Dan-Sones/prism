84902cd91fb8435b46d0ec3d4d67428b98327702
danielsones <daniel.sones123@gmail.com>
2026-05-02 12:06:13 +0100

    fix: update query builder to query from cooked_events

--- services/experimentation-service/internal/service/clickhouseQueryBuilder_test.go
+++ services/experimentation-service/internal/service/clickhouseQueryBuilder_test.go
@@ -186,7 +186,7 @@ func TestClickhouseQueryBuilder_BuildQueryForExperimentMetric_Ratio(t *testing.T
 					},
 				},
 			},
-			expectedQuery: `SELECT variant_key, uniqExactIf(user_id, event_key = 'purchase') AS numerator, uniqExactIf(user_id, event_key = 'experiment_exposure') AS denominator FROM events WHERE experiment_key = 'button_color_v1' AND event_key IN ('purchase', 'experiment_exposure') AND sent_at >= '2026-04-23 14:30:00' AND sent_at <= '2026-04-23 15:30:00' GROUP BY variant_key;`,
+			expectedQuery: `SELECT variant_key, uniqExactIf(user_id, event_key = 'purchase') AS numerator, uniqExactIf(user_id, event_key = 'experiment_exposure') AS denominator FROM cooked_events WHERE experiment_key = 'button_color_v1' AND event_key IN ('purchase', 'experiment_exposure') AND sent_at >= '2026-04-23 14:30:00' AND sent_at <= '2026-04-23 15:30:00' GROUP BY variant_key;`,
 		},
 	}
 
