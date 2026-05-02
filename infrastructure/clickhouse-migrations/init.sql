e36505806cdd37af390643bd79e508862c26ae85
danielsones <daniel.sones123@gmail.com>
2026-05-02 10:58:28 +0100

    feat: repurpose clickhouse writer as write through for raw events

--- infrastructure/clickhouse-migrations/init.sql
+++ infrastructure/clickhouse-migrations/init.sql
@@ -11,3 +11,15 @@ CREATE TABLE cooked_events (
     is_aa bool
 ) ENGINE = MergeTree()
 ORDER BY (experiment_key, variant_key, event_key, received_at);
+
+
+CREATE TABLE events (
+    event_key         String,
+    user_id           String,
+    sent_at           DateTime,
+    received_at       DateTime,
+    string_properties Map(String, String),
+    int_properties    Map(String, Int64),
+    float_properties  Map(String, Float64)
+) ENGINE = MergeTree()
+      ORDER BY (received_at, event_key);
