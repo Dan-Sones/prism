167f6e5f70723029da2aa4b113828b83452758b1
danielsones <daniel.sones123@gmail.com>
2026-05-08 17:26:34 +0100

    chore: experiment result models

---
+++ libs/prismdbmodels/model/experimentResults/enrichedExperimentResults.go
@@ -0,0 +1,14 @@
+package experimentResults
+
+import (
+	"github.com/Dan-Sones/prismdbmodels/model/experiment"
+	"github.com/google/uuid"
+)
+
+type EnrichedExperimentResults struct {
+	DecisionRecommendation DecisionRecommendation                            `json:"decision_recommendation"`
+	RecommendationReason   string                                            `json:"recommendation_reason"`
+	TestResults            map[uuid.UUID]ZTestResult                         `json:"test_results"`
+	Metrics                map[uuid.UUID]experiment.EnrichedExperimentMetric `json:"metrics"`
+	MetricValues           map[uuid.UUID]MetricValue                         `json:"metric_observations"`
+}
