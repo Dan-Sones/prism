4395f6b70cc96bc8ef32a6a46c7139d4fe3fef7d
danielsones <daniel.sones123@gmail.com>
2026-06-05 08:27:53 +0100

    chore: ramp load test

---
+++ tools/gatling-load-testing/src/test/java/example/AssignmentRampSimulation.java
@@ -0,0 +1,29 @@
+package example;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import java.time.Duration;
+import java.util.*;
+
+public class AssignmentRampSimulation extends Simulation {
+    HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8082").acceptHeader("application/json");
+
+    ScenarioBuilder scn = scenario("Ramp scenario")
+            .exec(http("Get endpoint")
+                    .get(session -> "/api/assignments/" + UUID.randomUUID().toString()));
+    ;
+
+    {
+        setUp(scn.injectOpen(
+                        incrementUsersPerSec(200)
+                                .times(10)
+                                .eachLevelLasting(Duration.ofSeconds(90))
+                                .separatedByRampsLasting(Duration.ofSeconds(10))
+                                .startingFrom(200)
+                        ))
+                .protocols(httpProtocol);
+    }
+}
